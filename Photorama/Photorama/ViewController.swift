8cc6f47a8c28adecdbbc59cd8bf06fb4991dfb69
gon <valerianaman@gmail.com>
2023-11-20 15:31:57 +0100

    Initial commit

---
+++ Photorama/Photorama/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Photorama
+//
+//  Created by Gonzalo Benítez Bueno on 20/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
